428f79a7cf41e5d0b3f87bcc28c7b0f0564d1011
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-05-06 00:48:59 +0100

    finito

--- zafirsphotography/Assets/JS/index.js
+++ zafirsphotography/Assets/JS/index.js
@@ -1,12 +1,16 @@
+document.getElementById("currentYear").innerText = new Date().getFullYear();
 
+// navbar
 
-  // footer year current
+document.addEventListener("DOMContentLoaded", function() {
+  const navbarLinks = document.querySelectorAll(".navbar-nav .nav-item");
 
-  document.addEventListener("DOMContentLoaded", function() { 
-    // get the the span element
-    const yrSpan = document.querySelector('.year');
-    // get the current year
-    const currentYr = new Date().getFullYear();
-    // set the year span element's text to the current year
-    yrSpan.textContent = currentYr;
+  navbarLinks.forEach(link => {
+      link.addEventListener("click", e => {
+          e.preventDefault();
+          const targetId = e.target.getAttribute("href").substring(1);
+          const targetSection = document.getElementById(targetId);
+          targetSection.scrollIntoView({ behavior: "smooth" });
+      });
   });
+});
