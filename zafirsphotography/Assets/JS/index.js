cec2816f2a8cb28156f14cd0d9ab95cbfb1ede38
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-05-08 00:44:07 +0100

    new look

--- zafirsphotography/Assets/JS/index.js
+++ zafirsphotography/Assets/JS/index.js
@@ -2,15 +2,33 @@ document.getElementById("currentYear").innerText = new Date().getFullYear();
 
 // navbar
 
-document.addEventListener("DOMContentLoaded", function() {
-  const navbarLinks = document.querySelectorAll(".navbar-nav .nav-item");
+// content scroll
 
-  navbarLinks.forEach(link => {
-      link.addEventListener("click", e => {
-          e.preventDefault();
-          const targetId = e.target.getAttribute("href").substring(1);
-          const targetSection = document.getElementById(targetId);
-          targetSection.scrollIntoView({ behavior: "smooth" });
+// update navbar
+
+$(document).ready(function() {
+  // Highlight active section in navbar
+  $(window).on('scroll', function() {
+      var scrollPosition = $(window).scrollTop();
+
+      $('.section').each(function() {
+          var sectionOffset = $(this).offset().top - 50; // Adjusted offset to trigger earlier
+          var sectionHeight = $(this).outerHeight();
+          var sectionId = $(this).attr('id');
+          var navItem = $('.navbar-nav').find('[href="#' + sectionId + '"]');
+          
+          if (scrollPosition >= sectionOffset && scrollPosition < sectionOffset + sectionHeight) {
+              navItem.addClass('active');
+          } else {
+              navItem.removeClass('active');
+          }
       });
   });
 });
+
+
+function playAudio(audioUrl) {
+  var audio = new Audio(audioUrl);
+  audio.play();
+}
+
